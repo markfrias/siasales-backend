2f8fc8926aa0bc65a226314d5b4a38ee8b5f8e28
Eniel-hub <78539777+Eniel-hub@users.noreply.github.com>
2021-11-19 16:29:05 +0800

    Update app.js
    add my codes

--- app.js
+++ app.js
@@ -2,49 +2,67 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { SalesPerson } = require('./models/salesperson');
+const { appendFileSync } = require('fs');
+const passport = require('passport');
+const session = require('express-session');
+
+// const { SalesPerson } = require('./models/salesperson');
 const { Customer } = require('./models/customer');
 var customersRouter = require('./api-routes/customer.route');
 var salesOrderRouter = require('./api-routes/salesOrder.route');
-const { appendFileSync } = require('fs');
+const usersRouter = require('./api-routes/users.route');
 const salesOrder = require('./models/salesOrder');
 
 
-
 const app = express();
+
+//middlewares
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended : false}));
 
 
-
+//connection to db
 mongoose.connect('mongodb+srv://dbAdmin:Ah%23%21TV6H@cluster0.d1664.mongodb.net/randochatapp?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    
-       
 }).then(() => {
     console.log("Connected to DB");
 }).catch((error) => console.log(error));
 
-
+//starting the server
 app.listen(8000, () => console.log (`Server is running on port ${8000} ...`));
-app.post('/user', (req, res) => {
-    let newUser = {
-       firstName: "Anthony",
-       lastName: "Fauci",
-       userName: "afauci",
-       password: "2theworldnct",
-       phoneNumber: "992-725-4490" 
-    }
-    SalesPerson.create(newUser, (err, small) => {
-        if (err) return res.json({message: err, status: "Error"});
-        res.json({message: "Account saved", status: "Success"})
-    })
+
+// app.post('/user', (req, res) => {
+//     let newUser = {
+//        firstName: "Anthony",
+//        lastName: "Fauci",
+//        userName: "afauci",
+//        password: "2theworldnct",
+//        phoneNumber: "992-725-4490" 
+//     }
+//     SalesPerson.create(newUser, (err, small) => {
+//         if (err) return res.json({message: err, status: "Error"});
+//         res.json({message: "Account saved", status: "Success"})
+//     })
     
-});
+// });
+
+//Express Session
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false}
+}));
+
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 app.use('/customers', customersRouter);
 app.use('/customer', customersRouter);
 app.use('/salesorder', salesOrderRouter);
+app.use('/users/', usersRouter);
