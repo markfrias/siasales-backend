72b375ecb8419e891cc3b61c9e7438c8a7e7aff3
Eniel-hub <78539777+Eniel-hub@users.noreply.github.com>
2021-11-19 16:38:37 +0800

    Update customer.route.js
    ensure Authentication

--- api-routes/customer.route.js
+++ api-routes/customer.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const customerControllers = require('../controllers/customers.controller');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
-router.get("/", customerControllers.getUsers);
-router.post("/", customerControllers.addUser);
-router.get("/s", customerControllers.getUser);
+router.get("/", ensureAuthenticated, customerControllers.getUsers);
+router.post("/", ensureAuthenticated, customerControllers.addUser);
+router.get("/s", ensureAuthenticated, customerControllers.getUser);
 
 module.exports = router
