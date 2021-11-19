59f5723d089399ec20079a9a08a20f99d850b43d
Mark Jabel Frias <friasmarkjabel@gmail.com>
2021-11-19 19:25:44 +0800

    Remove unnecessary dependencies

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const { SalesPerson } = require('./models/salesperson');
 const { Customer } = require('./models/customer');
 var customersRouter = require('./api-routes/customer.route');
 var salesOrderRouter = require('./api-routes/salesOrder.route');
-const { appendFileSync } = require('fs');
+var salesRouter = require('./api-routes/sales.route');
 const salesOrder = require('./models/salesOrder');
 
 
@@ -29,22 +28,9 @@ mongoose.connect('mongodb+srv://dbAdmin:Ah%23%21TV6H@cluster0.d1664.mongodb.net/
 
 
 app.listen(8000, () => console.log (`Server is running on port ${8000} ...`));
-app.post('/user', (req, res) => {
-    let newUser = {
-       firstName: "Anthony",
-       lastName: "Fauci",
-       userName: "afauci",
-       password: "2theworldnct",
-       phoneNumber: "992-725-4490" 
-    }
-    SalesPerson.create(newUser, (err, small) => {
-        if (err) return res.json({message: err, status: "Error"});
-        res.json({message: "Account saved", status: "Success"})
-    })
-    
-});
 
 // Routes
 app.use('/customers', customersRouter);
 app.use('/customer', customersRouter);
 app.use('/salesorder', salesOrderRouter);
+app.use('/sales', salesRouter);
