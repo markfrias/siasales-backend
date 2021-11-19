860b6707eaf6ca67b1865317a52256818f53e994
Eniel-hub <78539777+Eniel-hub@users.noreply.github.com>
2021-11-19 16:15:26 +0800

    Update salesperson.js
    change the module.exports

--- models/salesperson.js
+++ models/salesperson.js
@@ -28,6 +28,4 @@ const salesPersonSchema = new Schema({
 })
 const SalesPerson = mongoose.model ('SalesPerson', salesPersonSchema)
 
-module.exports = {
-    SalesPerson: SalesPerson
-}
+module.exports = SalesPerson
