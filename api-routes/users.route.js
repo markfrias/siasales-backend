1610a62ec4532b40f7ca9d33fd145f6e859b7c67
Eniel-hub <78539777+Eniel-hub@users.noreply.github.com>
2021-11-23 23:25:51 +0800

    Update users.route.js

--- api-routes/users.route.js
+++ api-routes/users.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router;
 const usersController = require('../controllers/users.controller');
 
-router.post('/login', usersController.login());
+router.get('/login', usersController.login());
 router.post('/register', usersController.register());
 router.get('/logout', usersController.logout());
 
